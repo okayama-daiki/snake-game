2993632941f51f1e5f9d0e102f56f932d1a77ba3
dike-okayama <lawliet.d.k.m@gmail.com>
2023-12-17 00:11:40 +0900

    Read URI from environment variables

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,8 +2,6 @@ import { useEffect, useRef } from "react";
 import GameEngine from "../services/GameEngine";
 import styles from "./Game.module.scss";
 
-const URI = "ws://0.0.0.0:8000";
-
 export default function Game({ toLobby }: { toLobby: () => void }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -13,7 +11,7 @@ export default function Game({ toLobby }: { toLobby: () => void }) {
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    new GameEngine(canvas, ctx, URI);
+    new GameEngine(canvas, ctx, import.meta.env.VITE_WSS_URI);
 
     toLobby;
   }, [canvasRef]);
