55c510c3d7cc84625d1ecf86127ce670c849d850
dike-okayama <lawliet.d.k.m@gmail.com>
2023-12-18 23:43:37 +0900

    Define Connecting status and implement an animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Lobby from "./components/Lobby";
 import Game from "./components/Game";
+import { ConnectionStatus, PlayerStatus } from "./types";
+
+const socket = new WebSocket(import.meta.env.VITE_WSS_URI);
 
 export default function App() {
-  const [isGameStarted, setIsGameStarted] = useState(false);
-  const toGame = () => setIsGameStarted(true);
-  const toLobby = () => setIsGameStarted(false);
+  const [playerStatus, setPlayerStatus] = useState(PlayerStatus.NOT_PLAYING);
+  const [connectionStatus, setConnectionStatus] = useState(
+    ConnectionStatus.CONNECTING
+  );
+
+  useEffect(() => {
+    socket.onopen = () => {
+      setConnectionStatus(ConnectionStatus.OPEN);
+    };
+    socket.onclose = () => {
+      setConnectionStatus(ConnectionStatus.CLOSED);
+    };
+  });
 
   return (
     <main>
-      {isGameStarted ? <Game toLobby={toLobby} /> : <Lobby toGame={toGame} />}
+      {playerStatus == PlayerStatus.PLAYING && (
+        <Game
+          socket={socket}
+          toLobby={() => setPlayerStatus(PlayerStatus.NOT_PLAYING)}
+        />
+      )}
+      {playerStatus == PlayerStatus.NOT_PLAYING && (
+        <Lobby
+          connectionStatus={connectionStatus}
+          toGame={() => setPlayerStatus(PlayerStatus.PLAYING)}
+        />
+      )}
     </main>
   );
 }
--- src/components/Lobby.tsx
+++ src/components/Lobby.tsx
@@ -1,10 +1,44 @@
+import { ConnectionStatus } from "../types";
 import styles from "./Lobby.module.scss";
 
-export default function Lobby({ toGame }: { toGame: () => void }) {
+export default function Lobby({
+  connectionStatus,
+  toGame,
+}: {
+  connectionStatus: ConnectionStatus;
+  toGame: () => void;
+}) {
   return (
-    <div className={styles.container} onClick={toGame}>
+    <div
+      className={styles.container}
+      onClick={() => {
+        connectionStatus === ConnectionStatus.OPEN && toGame();
+      }}
+    >
       <h1 className={styles.title}>Snake Game</h1>
-      <p className={styles.description}>Tap to Start</p>
+      <p className={styles.message}>
+        {connectionStatus === ConnectionStatus.CONNECTING && (
+          <>
+            <span className={styles.fadeIn}>Connecting</span>
+            <SnakeLoader />
+          </>
+        )}
+        {connectionStatus === ConnectionStatus.OPEN && (
+          <span className={styles.blink}>Tap to Start</span>
+        )}
+      </p>
     </div>
   );
 }
+
+const SnakeLoader = () => {
+  return (
+    <span className={styles.loader}>
+      {Array(4)
+        .fill(null)
+        .map((_, i) => {
+          return <span key={i} className={styles.cell}></span>;
+        })}
+    </span>
+  );
+};
