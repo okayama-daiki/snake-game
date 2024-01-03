d04f03862e01174b71b83b6cb5651a582ed12ba2
dike-okayama <lawliet.d.k.m@gmail.com>
2024-01-03 12:14:19 +0900

    Enable SSL/TLS

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod websocket_session;
 use actix::{Actor, Addr};
 use actix_web::{get, web::Data, web::Payload, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use std::env;
 use websocket_actor::WebsocketActor;
 use websocket_session::WebsocketSession;
@@ -30,17 +31,41 @@ async fn main() -> std::io::Result<()> {
     };
     let port = match env::var("PORT") {
         Ok(val) => val,
-        Err(_e) => "8000".to_string(),
+        Err(_e) => "5173".to_string(),
     };
 
     let websocket_server = WebsocketActor::default().start();
 
-    HttpServer::new(move || {
-        App::new()
-            .service(handle_connection)
-            .app_data(Data::new(websocket_server.clone()))
-    })
-    .bind(format!("{}:{}", host, port))?
-    .run()
-    .await
+    if std::env::var("PRIVATE_KEY_FILE").is_err()
+        || std::env::var("CERTIFICATE_CHAIN_FILE").is_err()
+    {
+        HttpServer::new(move || {
+            App::new()
+                .service(handle_connection)
+                .app_data(Data::new(websocket_server.clone()))
+        })
+        .bind(format!("{}:{}", host, port))?
+        .run()
+        .await
+    } else {
+        let private_key = std::env::var("PRIVATE_KEY_FILE").unwrap();
+        let certificate_chain = std::env::var("CERTIFICATE_CHAIN_FILE").unwrap();
+
+        let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+        builder
+            .set_private_key_file(private_key, SslFiletype::PEM)
+            .unwrap();
+        builder
+            .set_certificate_chain_file(certificate_chain)
+            .unwrap();
+
+        HttpServer::new(move || {
+            App::new()
+                .service(handle_connection)
+                .app_data(Data::new(websocket_server.clone()))
+        })
+        .bind_openssl(format!("{}:{}", host, port), builder)?
+        .run()
+        .await
+    }
 }
